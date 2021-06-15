1b90f3e7ab021b2584c924454d991d7e5754c363
DeriWars <50499055+DeriWars@users.noreply.github.com>
2021-06-15 17:11:42 +0200

    Fixed main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,8 @@
 const int TRIG = 13; // Assigne la valeur du pin TRIGGER
 const int ECHO = 11; // Assigne la valeur du pin ECHO
 
-const int TCRT_AVANT = A0; // Assigne la valeur du pin assigner au détecteur avant
-const int TCRT_ARRIERE = A1; // Assigne la valeur du pin assigne au détecteur arrière
+const int TCRT_AVANT = A1; // Assigne la valeur du pin assigner au détecteur avant
+const int TCRT_ARRIERE = A0; // Assigne la valeur du pin assigne au détecteur arrière
 
 const int MOTEUR_G_PWM = 9;  // Assigne la valeur du pin assigner au PWM des moteurs gauches
 const int MOTEUR_G_DIR_1 = 7; // Assigne la valeur du pin assigner à la direction 1 des moteurs gauches
@@ -15,18 +15,28 @@ const int MAX_DISTANCE = 77; // The maximum distance that the enemy can be
 const int ATTACK_DISTANCE = 20; // The distance where the sumobot start attacking
 const int MIN_DISTANCE = 2; // The minimum distance with the enemy to engage evasive manoeuvers
 
+const int PUSH = 8;
+
+const int FULL_SPEED = 255;
+const int MAX_SPEED = 125;
 const int SPEED = 80;
-const int MIN_SPEED = 30;
+const int MIN_SPEED = 50;
+
+const int WHITE_LINE_MIN_VALUE = 800;
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
+
+  Serial.println("ETALP - STARTING...");
   
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
 
   pinMode(TCRT_AVANT, INPUT); // Initialise le pin TCRT_AVANT comme entrée
   pinMode(TCRT_ARRIERE, INPUT); // Initialise le pin TCRT_ARRIERE comme entrée
+
+  pinMode(PUSH, INPUT_PULLUP);
 
   pinMode(MOTEUR_G_PWM, OUTPUT); // Initalise le pin MOTEUR_G_PWM comme sortie
   pinMode(MOTEUR_G_DIR_1, OUTPUT); // Idem avec MOTEUR_G_DIR_1
@@ -35,9 +45,17 @@ void setup() {
   pinMode(MOTEUR_D_DIR_1, OUTPUT); // Idem avec MOTEUR_D_DIR_1
   pinMode(MOTEUR_D_DIR_2, OUTPUT); // Idem avec MOTEUR_D_DIR_2
 
-  //interrupts();
-  //attachInterrupt(TCRT_AVANT, forwardLine, RISING);
-  //attachInterrupt(TCRT_ARRIERE, backwardLine, RISING);
+  interrupts();
+  attachInterrupt(TCRT_AVANT, forwardLine, RISING);
+  attachInterrupt(TCRT_ARRIERE, backwardLine, RISING);
+
+  Serial.println("ETALP - Started!");
+
+  while(digitalRead(PUSH)){
+    delay(1);
+  }
+
+  Serial.println("ETALP - LAUNCHING...");
 }
 
 void loop() {
@@ -61,14 +79,13 @@ void loop() {
 
 
 int checkWhiteLine(){
-  /*if(digitalRead(TCRT_AVANT) && digitalRead(TCRT_ARRIERE)){
-    Serial.println("LOSER!");
+  if(analogRead(TCRT_AVANT) >= WHITE_LINE_MIN_VALUE && analogRead(TCRT_ARRIERE) >= WHITE_LINE_MIN_VALUE){
     return 2;
-  }else if(digitalRead(TCRT_AVANT)){
+  }else if(analogRead(TCRT_AVANT) >= WHITE_LINE_MIN_VALUE){
     return 1;
-  }else if(digitalRead(TCRT_ARRIERE)){
+  }else if(analogRead(TCRT_ARRIERE) >= WHITE_LINE_MIN_VALUE){
     return -1;
-  }*/
+  }
   
   return 0;
 }
@@ -85,18 +102,18 @@ void engageEvasiveManeuver(int maneuver){
   stopEngine();
   
   if(maneuver == -1){ // Back sensor has detected white line
-    rightEngine(MIN_SPEED, 1, 0);
-    leftEngine(SPEED, 1, 0);
+    rightEngine(FULL_SPEED, 1, 0);
+    leftEngine(FULL_SPEED, 1, 0);
     Serial.println("Back sensors have detected somethings!");
-    delay(3000);
+    delay(1000);
     stopEngine();
   } else if(maneuver == 1){ // Forward sensors have detected white line
-    rightEngine(MIN_SPEED, 0, 1);
-    leftEngine(SPEED, 0, 1);
+    rightEngine(FULL_SPEED, 0, 1);
+    leftEngine(FULL_SPEED, 0, 1);
     Serial.println("Forward sensors have detected somethings!");
-    delay(3000);
+    delay(1000);
     stopEngine();
-  } else {
+  } else if(maneuver == 2) {
     startRotationRight(SPEED);
     Serial.println("BOTH sensors have detected somethings!");
     delay(500);
@@ -120,27 +137,30 @@ boolean findEnemy() {
     
     if(!maneuver && distance > MIN_DISTANCE){
       engageEvasiveManeuver(maneuver);
+    } else if(maneuver != 0){
+      engageEvasiveManeuver(maneuver);
+      startRotationLeft(MIN_SPEED);
+      detectionTime = -1;
+      lastDetectionTime = -1;
+      timeElapsed = -1;
     }
 
     if(timeElapsed == -1){
-      if(distance <= 0){
-        Serial.print("Detection error! Distance is ");
-        Serial.print(distance);
-        Serial.println(" cm.");
-      } else if(distance > MAX_DISTANCE && lastDetectionTime > 0){
+      if((distance > MAX_DISTANCE || distance <= 0) && lastDetectionTime > 0){
         timeElapsed = (int)(lastDetectionTime - detectionTime);
-        Serial.print("Enemy detected! Centering...");
-      } else {
+        timeElapsed = (timeElapsed < 0) ? -timeElapsed : timeElapsed;
+      } else if(distance > 0) {
         if(detectionTime == -1){
           detectionTime = millis();
           Serial.println("Enemy detected! (Start)");
-        } else{
-          lastDetectionTime = millis();
-          Serial.println("Enemy detected! (End)");
         }
+      } else{
+        timeElapsed = (int)(lastDetectionTime - detectionTime);
+        Serial.print("Enemy detected! Centering...");
       }
     } else{
       stopEngine();
+      delay(1000);
       centerPosition(timeElapsed);
       return true;
     }
@@ -167,59 +187,36 @@ float detect() {
 }
 
 void centerPosition(int timeElapsed){
-  startRotationRight(255);
+  Serial.println("ETALP - Centering...");
+  startRotationRight(MIN_SPEED);
+  Serial.println(timeElapsed);
   delay((int)(timeElapsed / 2));
   stopEngine();
+  Serial.println("ETALP - Centered!");
 }
 
 
 
-void attack(){  
-  int startTime = millis();
-  int startDistance = detect();
-  int distance = startDistance;
+void attack(){
+  Serial.println("ETALP - Attacking ennemy...");
+  forward(SPEED);
 
-  Serial.println("Attacking ennemy...");
-  
-  while(distance > ATTACK_DISTANCE){
+  float distance = detect();
+
+  while(distance > 0 && distance <= MAX_DISTANCE){
+    delay(200);
     distance = detect();
 
-    if(distance > MAX_DISTANCE){ // Enemy has been lost
-      forward(255);
-      Serial.println("Enemy lost!");
-      
-      while(detect() && !checkWhiteLine()){
-        delay(100);
-      }
-      
-      stopEngine();
-      return;
+    int maneuver = checkWhiteLine();
+
+    if(maneuver != 0){
+      engageEvasiveManeuver(maneuver);
     }
   }
-  
-  int endTime = millis();
-  float speed = (distance - startDistance) / (endTime - startTime);
 
-  //Enemy go in face-to-face
-  startRotationLeft(255);
-  delay(1000);
-  rightEngine(200, 1, 0);
-  leftEngine(255, 1, 0);
-
-  int maneuverTime;
-
-  while(maneuverTime < 2000){
-    maneuverTime += 10;
-    int whiteLines = checkWhiteLine();
-
-    if(!whiteLines){
-      delay(10);
-      continue;
-    }
-
-    engageEvasiveManeuver(whiteLines);
-    return;
-  }
+  backward(FULL_SPEED);
+  delay(200);
+  stopEngine();
 }
 
 
@@ -235,13 +232,13 @@ void backward(int speed){
 }
 
 void startRotationLeft(int speed){
-  leftEngine(speed, 1, 0);
-  rightEngine(speed, 0, 1);
+  leftEngine(speed, 0, 1);
+  rightEngine(speed, 1, 0);
 }
 
 void startRotationRight(int speed){
-  rightEngine(speed, 1, 0);
-  leftEngine(speed, 0, 1);
+  rightEngine(speed, 0, 1);
+  leftEngine(speed, 1, 0);
 }
 
 void rightEngine(int speed, int direction, int opposite){
